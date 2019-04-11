9ecdeba7fdce7b9c7160f92e005e4a76efb6c5a6
程宝生 <chenbaosheng@build4dream.com>
2019-04-11 21:35:58 +0800

    城市页面的布局

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="list">
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~Styles/varibles.styl'
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    top: 1.66rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    .item
+      text-align: center
+      line-height: .4rem
+      color: $bgColor
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper current-city">
+            <div class="button">杭州</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">杭州</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">杭州</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">杭州</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">杭州</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+          <div class="item border-bottom">阿尔多</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+
+export default {
+  name: 'CityList',
+  mounted () {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~Styles/varibles.styl'
+  .border-topbottom
+    &:after
+      border-color: #ccc
+    &:before
+      border-color: #ccc
+  .border-bottom
+    &:before
+      border-color: #ccc
+  .list
+    overflow: hidden
+    position: absolute
+    top: 1.66rem
+    left: 0
+    right: 0
+    bottom: 0
+    .title
+      line-height: .54rem
+      background: #f5f5f5
+      color: #212121
+      font-size: .26rem
+      padding-left: .2rem
+    .button-list
+      overflow: hidden
+      padding: .1rem .6rem .1rem .1rem
+      .button-wrapper
+        float: left
+        width: 33.33%
+        &.current-city
+          .button
+            border-color: $bgColor
+            color: $bgColor
+        .button
+          margin: .1rem
+          padding: .1rem 0
+          text-align: center
+          border: .02rem solid #ccc
+          border-radius: .06rem
+          color: #333
+    .item-list
+      .item
+        line-height: .76rem
+        padding-left: .2rem
+</style>

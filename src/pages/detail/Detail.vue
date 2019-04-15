7261a1a18a294998afb9eb432ee36cd020b465e6
程宝生 <chenbaosheng@build4dream.com>
2019-04-15 13:53:19 +0800

    详情递归组件列表

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,6 +2,7 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
+    <detail-list :list="list"></detail-list>
     <div class="content"></div>
   </div>
 </template>
@@ -9,12 +10,30 @@
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '景点票'
+        }, {
+          title: '游玩票'
+        }]
+      }, {
+        title: '儿童票'
+      }, {
+        title: '免费票'
+      }]
+    }
   }
 }
 </script>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="item" v-for="(item, index) of list" :key="index">
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>
+        {{item.title}}
+      </div>
+      <div v-if="item.children" class="item-children">
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .item
+    .item-title
+      height: .8rem
+      line-height: .8rem
+    .item-title-icon
+      position: relative
+      left: .06rem
+      top: .1rem
+      display: inline-block
+      width: .36rem
+      height: .36rem
+      background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+      margin-right: .1rem
+      background-size: .4rem 3rem
+    .item-children
+      padding-left: .2rem
+</style>
